26d9d115efbcc6864f2dfad9d6df310011fea311
Hufan <hufany21@gmail.com>
2021-11-27 19:36:34 -0500

    changed render back to index

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Separate them into separate routes files (see above).
 
 app.get("/", (req, res) => {
-  res.render("login");
+  res.render("index");
 });
 
 
