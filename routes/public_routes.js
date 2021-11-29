8062530af289d480072b55da7ba9c759516eef04
charit nanduri <charithnanduri03@gmail.com>
2021-11-29 18:10:15 +0000

    home page modified and login and index routes added to specific route page

--- routes/public_routes.js
+++ routes/public_routes.js
@@ -10,18 +10,13 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    let query = `SELECT * FROM widgets`;
-    console.log(query);
-    db.query(query)
-      .then(data => {
-        const widgets = data.rows;
-        res.json({ widgets });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
+    res.render("index");
+    // render product template to visualize front end changes -- switch back to index after***
   });
+
+  router.get("/login", (req, res) => {
+    res.render("login");
+  });
+
   return router;
-};
+}
