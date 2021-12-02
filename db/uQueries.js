d4189877e67c5b43c6c9c64c54fbf813f8ee6d00
charit nanduri <charithnanduri03@gmail.com>
2021-12-02 00:05:30 +0000

    add product route and render products dynamically

--- db/uQueries.js
+++ db/uQueries.js
@@ -9,8 +9,16 @@ const getUsersProducts = (users_id) => {
     })
 };
 
+const getProductById = (product_id) => {
+  return pool
+  .query(`SELECT * FROM PRODUCTS WHERE id = $1`,[product_id])
+  .then(result => {
+    return result.rows[0];
+  })
+}
+
 const getUsersFavourites = (user_id) => {
-  return pool.query('SELECT title, price, description, image, date_created, users.name AS name FROM products JOIN favourites ON product_id = products.id JOIN users ON user_id = users.id WHERE favourites.user_id = $1 GROUP BY favourites.user_id, products.id, users.name;', [user_id])
+  return pool.query('SELECT products.id, title, price, description, image, date_created, users.name AS name FROM products JOIN favourites ON product_id = products.id JOIN users ON user_id = users.id WHERE favourites.user_id = $1 GROUP BY favourites.user_id, products.id, users.name;', [user_id])
   .then((result) => result.rows)
     .catch(err => {
         console.log(err.message)
@@ -61,5 +69,6 @@ module.exports = {
   deleteUsersProduct,
   markAsSold,
   addFavourite,
-  newProduct
+  newProduct,
+  getProductById
 }
